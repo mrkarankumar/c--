41d300bc9d7175c6e9fe8682d862d4aee7963800
Karan Kumar <149156353+mrkarankumar@users.noreply.github.com>
2023-12-30 00:52:46 +0530

    c and c++ commit

---
+++ karan.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<" hi amin ansari kya hal  bro kuch kam kiya ke  nahi";
+
+    return 0;
+}
---
+++ karan1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("karan kumar chaudhary ");
+
+    return 0;
+}
