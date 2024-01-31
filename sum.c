add75540209c4406d3cde25d322cdbf1022aa62f
Karan Kumar <149156353+mrkarankumar@users.noreply.github.com>
2024-02-01 00:30:47 +0530

    c  arthmeric  operator

---
+++ sum.c
@@ -0,0 +1,281 @@
+// #include <stdio.h>
+
+// int main() {
+
+// int karan1 = 200 + 400;
+// int sonu2 = karan1 + 600;
+// int simran3 = karan1 +sonu2;
+// printf("%d\n",karan1);
+
+// printf("%d\n",sonu2);
+// printf("%d\n",simran3);
+// return 0;
+
+
+// }
+
+//aadition //
+
+
+// #include <stdio.h>
+
+// int main(){
+
+
+// int karan = 10;
+// int amin =  30;
+// printf("%d", karan+amin);
+
+// return 0;
+
+// }
+
+
+//subtrac //
+
+
+// #include <stdio.h>
+
+// int main(){
+
+
+// int aman = 100;
+// int amin =  30;
+// printf("%d", aman-amin);
+
+// return 0;
+
+// }
+
+// multiplacton //
+
+// #include <stdio.h>
+
+// int main(){
+
+
+// int karan = 1000;
+// int amin =  3000;
+// printf("%d", karan*amin);
+
+// return 0;
+
+// }
+
+
+// division //
+
+
+// #include <stdio.h>
+
+// int main() {
+
+// int karan1 = 200 + 400;
+// int sonu2 = karan1 + 600;
+// int simran3 = karan1 +sonu2;
+// printf("%d\n",karan1);
+
+// printf("%d\n",sonu2);
+// printf("%d\n",simran3);
+// return 0;
+
+
+// }
+
+//aadition //
+
+
+// #include <stdio.h>
+
+// int main(){
+
+
+// int karan = 10;
+// int amin =  30;
+// printf("%d", karan+amin);
+
+// return 0;
+
+// }
+
+
+//subtrac //
+
+
+// #include <stdio.h>
+
+// int main(){
+
+
+// int aman = 100;
+// int amin =  30;
+// printf("%d", aman-amin);
+
+// return 0;
+
+// }
+
+// multiplacton //
+
+// #include <stdio.h>
+
+// int main(){
+
+
+// int karan = 100;
+// int amin =  2;
+// printf("%d", karan/amin);
+
+// return 0;
+
+// }
+
+
+// modulus //
+
+// #include <stdio.h>
+
+// int main() {
+
+// int karan1 = 200 + 400;
+// int sonu2 = karan1 + 600;
+// int simran3 = karan1 +sonu2;
+// printf("%d\n",karan1);
+
+// printf("%d\n",sonu2);
+// printf("%d\n",simran3);
+// return 0;
+
+
+// }
+
+//aadition //
+
+
+// #include <stdio.h>
+
+// int main(){
+
+
+// int karan = 10;
+// int amin =  30;
+// printf("%d", karan+amin);
+
+// return 0;
+
+// }
+
+
+//subtrac //
+
+
+// #include <stdio.h>
+
+// int main(){
+
+
+// int aman = 100;
+// int amin =  30;
+// printf("%d", aman-amin);
+
+// return 0;
+
+// }
+
+// modulus  //
+
+// #include <stdio.h>
+
+// int main(){
+
+
+// int karan = 13;
+// int amin =  6;
+// printf("%d", karan%amin);
+
+// return 0;
+
+// }
+
+
+
+// incement //
+
+// #include <stdio.h>
+
+// int main() {
+
+// int karan1 = 200 + 400;
+// int sonu2 = karan1 + 600;
+// int simran3 = karan1 +sonu2;
+// printf("%d\n",karan1);
+
+// printf("%d\n",sonu2);
+// printf("%d\n",simran3);
+// return 0;
+
+
+// }
+
+//aadition //
+
+
+// #include <stdio.h>
+
+// int main(){
+
+
+// int karan = 10;
+// int amin =  30;
+// printf("%d", karan+amin);
+
+// return 0;
+
+// }
+
+
+//subtrac //
+
+
+// #include <stdio.h>
+
+// int main(){
+
+
+// int aman = 100;
+// int amin =  30;
+// printf("%d", aman-amin);
+
+// return 0;
+
+// }
+
+// increment //
+
+// #include <stdio.h>
+
+// int main(){
+
+
+// int karan = 1000;
+// // int amin =  3000;
+// printf("%d", ++karan);
+
+// return 0;
+
+// }
+
+// decrement //
+
+// #include <stdio.h>
+
+// int main(){
+
+
+// int karan = 1000;
+// // int amin =  3000;
+// printf("%d", --karan);
+
+// return 0;
+
+// }
