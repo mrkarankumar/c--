796fb8c3145e470a428b9a39d47dfac16002af7e
Karan Kumar <149156353+mrkarankumar@users.noreply.github.com>
2024-02-01 01:21:04 +0530

    logical operator

---
+++ comprasion.c
@@ -0,0 +1,50 @@
+// #include <stdio.h>
+
+// int main(){
+
+// int b =4;
+// int k= 4;
+
+// printf("%d", b==k);
+
+// return 0;
+
+
+// }
+
+
+// not equal //
+
+// #include <stdio.h>
+
+// int main() {
+//   int x = 15;
+//   int y = 15;
+//   printf("%d", x != y); // returns 1 (true) because 5 is not equal to 3
+//   return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main(){
+
+// int a =22;
+// int b = 12;
+// printf("%d",a<b);
+// return 0;
+
+
+// }
+
+
+#include <stdio.h>
+
+int main(){
+
+int  a =30;
+int  b = 40 ;
+ printf("%d", b>a);
+
+
+}
---
+++ logical.c
@@ -0,0 +1,37 @@
+// #include <stdio.h>
+
+
+// int main(){
+
+// int a = 34;
+// int b = 35;
+
+//  printf("%d", a<4&& b<6);
+//  return 0;
+
+// }
+
+
+// #include <stdio.h>
+
+// int main(){
+
+// int a = 34;
+// int b = 35;
+
+//   printf("%d", a<25 || b<23);
+//   return 0;
+// }
+
+
+#include <stdio.h>
+int main(){
+
+int a = 34;
+int b = 35;
+
+printf("%d",!(a<23 && b<25));
+return 0;
+
+
+}
