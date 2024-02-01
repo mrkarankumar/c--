439ad77c313e3adcc6a35885113b435c2311d7a1
Karan Kumar <149156353+mrkarankumar@users.noreply.github.com>
2024-02-01 17:16:37 +0530

    commit

--- parameter1.c
+++ parameter1.c
@@ -5,7 +5,7 @@ void myFunction(int myNumbers[5]) {
 //     printf("%d\n", myNumbers[i]);
 //   }
 
-for (int i=0; i<5;i++){
+for (int i=0; i<10;i++){
 
 printf("%d\n",myNumbers[i]);
 
@@ -15,7 +15,7 @@ printf("%d\n",myNumbers[i]);
 }
 
 int main() { 
-  int myNumbers[5] = {10, 20, 30, 40, 50,60,70,80,90,100};  
+  int myNumbers[5] = {10, 20, 30, 40, 50,60,70,80,90,100,101};  
   myFunction(myNumbers);
   return 0;
 }
