3e2fc25c66175aa4e94f1c80593ba7c0abe2c801
Karan Kumar <149156353+mrkarankumar@users.noreply.github.com>
2024-02-01 17:16:12 +0530

    para commit

---
+++ parameter1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void myFunction(int myNumbers[5]) {
+//   for (int i = 0; i < 5; i++) {
+//     printf("%d\n", myNumbers[i]);
+//   }
+
+for (int i=0; i<5;i++){
+
+printf("%d\n",myNumbers[i]);
+
+}
+
+
+}
+
+int main() { 
+  int myNumbers[5] = {10, 20, 30, 40, 50,60,70,80,90,100};  
+  myFunction(myNumbers);
+  return 0;
+}
